7c73c1c2f1c7ada20bc945c1004891c718b6d2f5
Ricky <rickyanand2@gmail.com>
2025-03-28 20:45:57 +1100

    Update custom user to use email

---
+++ tp_sra/accounts/forms.py
@@ -0,0 +1,18 @@
+# accounts/forms.py
+from django.contrib.auth.forms import AdminUserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+
+class CustomUserCreationForm(AdminUserCreationForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ("username", "email")
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ("username", "email")
